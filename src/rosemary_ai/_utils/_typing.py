4ec186b46eb146895fd3b302ac7b4650482d9754
snw2015 <4564959@qq.com>
2024-07-17 18:13:26 +0900

    Minor fix

--- src/rosemary_ai/_utils/_typing.py
+++ src/rosemary_ai/_utils/_typing.py
@@ -4,7 +4,7 @@ from typing import Callable
 
 def add_parameter_to_func(f: Callable, param_name: str, param_type):
     signature = inspect.signature(f)
-    param = inspect.Parameter(param_name, inspect.Parameter.POSITIONAL_OR_KEYWORD, annotation=param_type)
+    param = inspect.Parameter(param_name, inspect.Parameter.POSITIONAL_OR_KEYWORD, annotation=param_type, default=None)
     parameters = list(signature.parameters.values()) + [param]
     new_signature = signature.replace(parameters=parameters)
 
