e493c8d7e3833f104f1a3c1efd4327f6ea548686
snw2015 <4564959@qq.com>
2024-07-17 18:59:44 +0900

    Default values of modified function signature

--- src/rosemary_ai/_utils/_typing.py
+++ src/rosemary_ai/_utils/_typing.py
@@ -2,9 +2,10 @@ import inspect
 from typing import Callable
 
 
-def add_parameter_to_func(f: Callable, param_name: str, param_type):
+def add_parameter_to_func(f: Callable, param_name: str, param_type, default_value=None):
     signature = inspect.signature(f)
-    param = inspect.Parameter(param_name, inspect.Parameter.POSITIONAL_OR_KEYWORD, annotation=param_type, default=None)
+    param = inspect.Parameter(param_name, inspect.Parameter.POSITIONAL_OR_KEYWORD,
+                              annotation=param_type, default=default_value)
     parameters = list(signature.parameters.values()) + [param]
     new_signature = signature.replace(parameters=parameters)
 
